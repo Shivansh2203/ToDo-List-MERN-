dc2785f5b88b60ac150898a6bfc67a90145811d8
Shivansh2203 <168472217+Shivansh2203@users.noreply.github.com>
2025-08-25 20:29:24 +0530

    Update Content component and ignore backend env

--- frontend/src/components/Content.jsx
+++ frontend/src/components/Content.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = "https://todo-list-mern-cor9.onrender.com"
 
 const Content = () => {
   const [task, setTask] = useState("");
